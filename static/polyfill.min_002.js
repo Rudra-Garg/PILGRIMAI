35bbff74717a34e6e1793c2d5f3de9ef9e10a997
Rudra Garg <121709697+Rudra-Garg@users.noreply.github.com>
2024-02-08 22:16:01 +0530

    Delete static directory

--- static/polyfill.min_002.js
+++
@@ -1,6 +0,0 @@
-/*
- * Polyfill service v3.25.1
- * Disable minification (remove `.min` from URL path) for more info
-*/
-
-(function(self, undefined) {Object.defineProperty(Array.prototype,"values",{value:Array.prototype[Symbol.iterator],enumerable:!1,writable:!1});})('object' === typeof window && window || 'object' === typeof self && self || 'object' === typeof global && global || {});
